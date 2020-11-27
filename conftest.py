56bf4596bbe4bd74ce191b1bcc9e93ead53e03e6
Mikhail Chirkov <mchirkov@ivi.ru>
2020-11-27 23:50:20 +0300

    Версия 4.1

--- conftest.py
+++ conftest.py
@@ -7,15 +7,19 @@ import json
 from selenium import webdriver
 
 
-def url_data_exists(url, timeout=15):
-    # Waiting for data to be available on given url
-    with allure.step(f"Waiting for resource availability {url}"):
-        while timeout:
-            response = requests.get(url)
-            if not response.ok:
-                time.sleep(1)
-                timeout -= 1
-        return timeout
+@allure.step("Waiting for resource availability {url}")
+def url_data(url, timeout=10):
+    while timeout:
+        response = requests.get(url)
+        if not response.ok:
+            time.sleep(1)
+            timeout -= 1
+        else:
+            if 'video' in url:
+                return response.content
+            else:
+                return response.text
+    return None
 
 
 @pytest.hookimpl(tryfirst=True, hookwrapper=True)
@@ -31,11 +35,11 @@ def pytest_runtest_makereport(item, call):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", default="chrome")
-    parser.addoption("--executor", default="localhost")
+    parser.addoption("--executor", default="127.0.0.1")
     parser.addoption("--bversion", action="store", default="86.0")
     parser.addoption("--vnc", action="store_true", default=False)
     parser.addoption("--logs", action="store_true", default=False)
-    parser.addoption("--video", action="store_true", default=False)
+    parser.addoption("--video", action="store_true", default=True)
 
 
 @pytest.fixture
@@ -45,7 +49,8 @@ def remote_browser(request):
     logs = request.config.getoption("--logs")
     video = request.config.getoption("--video")
     version = request.config.getoption("--bversion")
-    executor_url = f"http://{request.config.getoption('--executor')}:4444/wd/hub"
+    executor = request.config.getoption('--executor')
+    executor_url = f"http://{executor}:4444/wd/hub"
 
     caps = {
         "browserName": browser,
@@ -63,49 +68,50 @@ def remote_browser(request):
         command_executor=executor_url
     )
 
-    # allure.attach(
-    #     name=driver.session_id,
-    #     body=json.dumps(driver.desired_capabilities),
-    #     attachment_type=allure.attachment_type.JSON)
+    # Attach browser data
+    allure.attach(
+        name=driver.session_id,
+        body=json.dumps(driver.desired_capabilities),
+        attachment_type=allure.attachment_type.JSON)
 
     def finalizer():
-        log_url = f"{executor_url}/logs/{driver.session_id}.log"
-        video_url = f"{executor_url}/video/{driver.session_id}.mp4"
+        log_url = f"{executor}/logs/{driver.session_id}.log"
+        video_url = f"http://{executor}:8080/video/{driver.session_id}.mp4"
         driver.quit()
 
-        # if request.node.status == 'failed':
-        #     if logs:
-        #         if url_data_exists(log_url):
-        #             allure.attach(
-        #                 name="selenoid_log_" + driver.session_id,
-        #                 body=requests.get(log_url).text,
-        #                 attachment_type=allure.attachment_type.TEXT)
-        #     if video:
-        #         if url_data_exists(video_url):
-        #             allure.attach(
-        #                 body=requests.get(video_url).content,
-        #                 name="video_for_" + driver.session_id,
-        #                 attachment_type=allure.attachment_type.MP4)
-        # else:
-        #     if video and url_data_exists(video_url): requests.delete(url=video_url)
-        #     if logs and url_data_exists(log_url): requests.delete(url=log_url)
-        #
-        # with open("allure-report/environment.xml", "w+") as file:
-        #     file.write(f"""<environment>
-        #         <parameter>
-        #             <key>Browser</key>
-        #             <value>{browser}</value>
-        #         </parameter>
-        #         <parameter>
-        #             <key>Browser.Version</key>
-        #             <value>{version}</value>
-        #         </parameter>
-        #         <parameter>
-        #             <key>Executor</key>
-        #             <value>{executor_url}</value>
-        #         </parameter>
-        #     </environment>
-        #     """)
+        if request.node.status != 'passed':
+            if logs:
+                allure.attach(
+                    name="selenoid_log_" + driver.session_id,
+                    body=url_data(log_url),
+                    attachment_type=allure.attachment_type.TEXT)
+            if video:
+                allure.attach(
+                    body=url_data(video_url),
+                    name="video_for_" + driver.session_id,
+                    attachment_type=allure.attachment_type.MP4)
+
+        # Clear videos and logs from selenoid
+        if video and url_data(video_url): requests.delete(url=video_url)
+        if logs and url_data(log_url): requests.delete(url=log_url)
+
+        # Add environment info to allure-report
+        with open("allure-report/environment.xml", "w+") as file:
+            file.write(f"""<environment>
+                <parameter>
+                    <key>Browser</key>
+                    <value>{browser}</value>
+                </parameter>
+                <parameter>
+                    <key>Browser.Version</key>
+                    <value>{version}</value>
+                </parameter>
+                <parameter>
+                    <key>Executor</key>
+                    <value>{executor_url}</value>
+                </parameter>
+            </environment>
+            """)
 
     request.addfinalizer(finalizer)
     return driver
