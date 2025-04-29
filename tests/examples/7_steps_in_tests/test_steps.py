c0b90d8f6a01036b61a562cf1c527dd36a026b7c
konflic <konflic@mail.ru>
2025-04-29 14:19:53 +0300

    4.31

--- tests/examples/7_steps_in_tests/test_steps.py
+++ tests/examples/7_steps_in_tests/test_steps.py
@@ -1,45 +1,56 @@
+from random import random
+import selectors
 import allure
 import pytest
+
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+import selenium.webdriver.support.expected_conditions as EC
 
 
-@allure.step("Выполняю клик по элементу {selector}")
-def click_element(driver, selector):
-    driver.find_element(By.CSS_SELECTOR, selector).click()
+class Page:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    @allure.step("Выполняю клик по элементу {selector}")
+    def click_element(self, selector):
+        self.driver.find_element(By.CSS_SELECTOR, selector).click()
+
+    @allure.step("Ввожу '{text}' в элемент {selector}")
+    def input_text(self, selector, text):
+        self.driver.find_element(By.CSS_SELECTOR, selector).send_keys(text)
+
+    @allure.step("Проверяю что элемент {selector} видим пользователю")
+    def check_visibility(self, selector):
+        WebDriverWait(self.driver, 3).until(EC.visibility_of_element_located((By.CSS_SELECTOR, selector)))
+
+    @allure.step("Открываю url {url}")
+    def open_page(self, url):
+        self.driver.get(url)
 
 
-@allure.step("Ввожу '{text}' в элемент {selector}")
-def input_text(driver, selector, text):
-    driver.find_element(By.CSS_SELECTOR, selector).send_keys(text)
+def test_step_example(driver):
+    page = Page(driver)
+    page.open_page("http://192.168.0.119:8081/")
+    page.input_text("#search input", "MacBook")
+    page.click_element("#search button")
+    page.check_visibility("#product-list")
 
 
-@allure.step("Открываю url {url}")
-def open_page(driver, url):
-    driver.get(url)
+@pytest.mark.parametrize("text", ["MacBook", "iPhone", "Canon"])
+def test_with_step_example(driver, text):
+    page = Page(driver)
+    input_field = "#search input"
 
-#
-# @allure.step("Выполняю тест")
-# def make_test(driver):
-#     open_page(driver, "https://ya.ru")
-#     input_text(driver, "#text", "Привет")
-#     click_element(driver, "[role='button']")
-
-
-def test_one(driver):
-    open_page(driver, "https://ya.ru")
-    input_text(driver, "#text", "Привет")
-    click_element(driver, "[role='button']")
-
-
-@pytest.mark.parametrize("text", ["Привет", "", 1234])
-def test_two(driver, text):
-    input_field = "#text"
-
-    with allure.step(f"Открыаю url и ввожу значение {input_field}"):
-        open_page(driver, "https://ya.ru")
+    with allure.step(f"Открыаю url и выполняю поиск {input_field}"):
+        page.open_page("http://192.168.0.119:8081/")
 
         with allure.step(f"Ввожу значение {text} в {input_field}"):
-            assert 0
+            page.input_text(input_field, text)
 
-            with allure.step("Нажимаю Сабмит"):
-                pass
+        with allure.step("Нажимаю Поиск"):
+            page.click_element("#search button")
+
+    with allure.step("Проверяю что отображаются результаты поиска"):
+        page.check_visibility("#product-list")
